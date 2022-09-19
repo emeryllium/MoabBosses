14b9e69f69b10da96874d6077d4c72aca328cb2a
emeryllium <72456735+emeryllium@users.noreply.github.com>
2022-09-19 12:21:29 -0700

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,8 +6,6 @@
         public const string Name = "Moab Bosses";
         public const string Description = "Turns all moab0 class bloons into boss bloons.";
 
-        public const string Icon = "BloonariusPortrait.png";
-
         public const string RepoOwner = "emeryllium";
         public const string RepoName = "MoabBosses";
     }
